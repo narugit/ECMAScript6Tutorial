dd1f60afa2ce480e33238dd0add79a825f793561
narugit <naruiroiro@gmail.com>
2018-07-07 09:58:31 +0900

    Using destructuring(分割代入).

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 let calculateMonthlyPayment = function (principal, years, rate) {
     let monthlyRate = 0;
     if (rate) {
-        zmonthlyRate = rate / 100 / 12;
+        monthlyRate = rate / 100 / 12;
     }
     let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+    return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
